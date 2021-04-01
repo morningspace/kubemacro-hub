93a71817a68104a25ae4f80422507ab17c897937
Ying Mo <morningspace@yahoo.com>
2021-04-01 23:06:30 +0800

    Update doc for delete-ns

--- macros/bin/delete-ns.sh
+++ macros/bin/delete-ns.sh
@@ -1,18 +1,24 @@
 ##
 # @Name: delete-ns
-# @Description: Delete a namespace.
+# @Description: Force delete a namespace.
 #
-# Delete a namespace.
+# This macro is designed to force delete a namespace that is stuck in `Terminating` status.
 #
-# Reference:
+# A namespace can keep `Terminating` for some reason. To force delete such a namespace, this
+# macro runs `kubectl delete` to trigger the deletion of the namespace if you haven't done so,
+# then clear the finalizers attached to the namespace.
+#
+# Some references about this idea can be found as below:
 # - https://stackoverflow.com/questions/55853312/how-to-force-delete-a-kubernetes-namespace
 # - https://github.com/kubernetes/kubernetes/issues/60807#issuecomment-572615776
 #
 # @Author: [morningspace](https://github.com/morningspace/)
 # @Usage: kubectl macro delete-ns (NAME) [options]
 # @Options:
+#   -h, --help: Print the help information.
+#       --version: Print the version information.
 # @Examples:
-#   # Delete a namespace.
+#   # Delete `foo` namespace.
 #   kubectl macro delete-ns foo
 # @Dependencies: tr,sed
 ##
@@ -20,13 +26,13 @@ function delete-ns {
   local ns=$1
   [[ -z $ns ]] && echo "You must specify a namespace." >&2 && return 1
 
+  # Try delete
+  kubectl delete ns $ns || return 1
+
   # Clear finalizer
   echo "Try to clear finalizer for $ns namespace..."
   kubectl get namespace $ns -o json | \
     tr -d "\n" | \
       sed "s/\"finalizers\": \[[^]]*\]/\"finalizers\": []/" | \
         kubectl replace --raw /api/v1/namespaces/$ns/finalize -f - || return 1
-
-  # Try delete and return immediately
-  kubectl delete ns deleteme --wait=false
 }
--- macros/docs/delete-ns.md
+++ macros/docs/delete-ns.md
@@ -1,6 +1,6 @@
 ## Macro: delete-ns
 
-Delete a namespace.
+Force delete a namespace.
 
 Author: [morningspace](https://github.com/morningspace/)
 
@@ -9,9 +9,13 @@ Author: [morningspace](https://github.com/morningspace/)
 ### **Description**
 
 
-Delete a namespace.
+This macro is designed to force delete a namespace that is stuck in `Terminating` status.
 
-Reference:
+A namespace can keep `Terminating` for some reason. To force delete such a namespace, this
+macro runs `kubectl delete` to trigger the deletion of the namespace if you haven't done so,
+then clear the finalizers attached to the namespace.
+
+Some references about this idea can be found as below:
 - https://stackoverflow.com/questions/55853312/how-to-force-delete-a-kubernetes-namespace
 - https://github.com/kubernetes/kubernetes/issues/60807#issuecomment-572615776
 
@@ -26,6 +30,8 @@ kubectl macro delete-ns (NAME) [options]
 **Options**
 
 ```
+ -h, --help: Print the help information.
+     --version: Print the version information.
 
 ```
 
@@ -33,7 +39,7 @@ kubectl macro delete-ns (NAME) [options]
 
 Here are some examples that you can take as reference to understand how to use this macro in practice.
 ```shell
-# Delete a namespace.
+# Delete `foo` namespace.
 kubectl macro delete-ns foo
 
 ```
